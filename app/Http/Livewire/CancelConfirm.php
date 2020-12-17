682e9c8d6e40eb7008aa26b1c29ec8180478db3b
quileab <quileab@gmail.com>
2020-12-17 20:38:28 -0300

    ConfirmModal Working

---
+++ app/Http/Livewire/CancelConfirm.php
@@ -0,0 +1,22 @@
+<?php
+
+namespace App\Http\Livewire;
+
+use Livewire\Component;
+
+class CancelConfirm extends Component
+{
+    public $color='purple'; // default if nothing
+    public $title='Title';
+    public $message="Empty";
+    public $cancelConfirmVisible=true;
+
+    public function alertOn(){
+        $this->cancelConfirmVisible=true;
+    }
+
+    public function render()
+    {
+        return view('livewire.cancel-confirm');
+    }
+}
--- resources/views/infocards/index.blade.php
+++ resources/views/infocards/index.blade.php
@@ -5,33 +5,10 @@
       </h2>
   </x-slot>
 
-  @php
-  $color="blue";
-  $title="Esto es una prueba";
-  $message="Esto es una prueba";
-  @endphp
+  @livewire('cancel-confirm',[
+    'title'=>'Live Alert',
+    'message'=>'Text of the message',
+    'color'=>'blue'
+    ])
 
-  <div class="max-w-6xl mx-auto sm:px-6 lg:px-8 text-white">
-      <h1>Esto es H1</h1>
-      <h2>Esto es H2</h2>
-      <h3>Esto es H3</h3>
-      <h4>Esto es H4</h4>
-      <h5>Esto es H5</h5>
-
-      <x-jet-button>Botón</x-jet-button>
-
-      <x-confirm-modal wire:model="alertVisible" color="{{ $color }}">
-          <x-slot name="title">{{ $title }}</x-slot>
-          <x-slot name="content">{{ $message }}</x-slot>
-
-          <x-slot name="footer">
-              <x-jet-secondary-button wire:click="$toggle('alertVisible')" wire:loading.attr="disabled">
-                  <span class="material-icons">clear</span> Cancelar
-              </x-jet-secondary-button>
-
-              <x-jet-danger-button class="ml-2" wire:click="deleteUser" wire:loading.attr="disabled">
-                  <span class="material-icons">delete_forever</span> Eliminar
-              </x-jet-danger-button>
-          </x-slot>
-      </x-confirm-modal>
 </x-app-layout>
