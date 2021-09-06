b37d5ea0a3ef30872f3f01dafb6b1d5437327114
quileab <quileab@gmail.com>
2021-09-06 20:49:18 -0300

    Roles & Permissions UI almost done

--- app/Http/Livewire/Permissions/PermissionComponent.php
+++ app/Http/Livewire/Permissions/PermissionComponent.php
@@ -3,7 +3,6 @@
 namespace App\Http\Livewire\Permissions;
 
 use Livewire\Component;
-use Illuminate\Support\Arr;
 use Spatie\Permission\Models\Role;
 use Spatie\Permission\Models\Permission;
 
@@ -11,30 +10,37 @@ class PermissionComponent extends Component
 {
     public $selectedpermissions=[];
     public $selectedrole;
+    public $newrole, $newpermission;
     public $checkall;
     protected $rules=
         ['selectedrole'=>['required','integer','exists:roles,id'],
         'selectedpermissions'=>['required','exists:permissions,id']
         ];
 
+    public function render()
+    {
+        $roles = Role::all();
+        $permissions = Permission::all()->sortBy('name');
+        //->pluck('name', 'id');
+        return view('livewire.permissions.index', compact('roles', 'permissions'));
+    }
+
     public function updatedSelectedRole($value)
     {
         $this->selectedpermissions=[];
         $role=Role::find($value);
         if($role) {
-            $this->selectedpermissions =$role->getAllPermissions()
-                 ->sortBy('name')
-                 ->pluck('name', 'id')
-                 ->toArray();
+            $this->selectedpermissions=$role->permissions->pluck('id','id')->toArray();
         }
     }
 
     public function Updatedcheckall($value)
     {
         if($value) {
-            $this->selectedpermissions = Permission::all()
-                ->pluck('name', 'id')
-                ->toArray();
+            $role=Role::find($value);
+            if ($role) {
+                $this->selectedpermissions=$role->permissions->pluck('id','id')->toArray();
+            }
         }
         else {
             $this->selectedpermissions=[];
@@ -45,26 +51,41 @@ class PermissionComponent extends Component
     {
         if($this->selectedpermissions){
             // remove unchecked values that comes with false assign it
-            $this->selectedpermissions = Arr::where($this->selectedpermissions, function ($value) {
-                return $value;
-            });
+            $this->selectedpermissions=array_filter($this->selectedpermissions);
         }
+
         $this->validate();
         $role=Role::find($this->selectedrole);
         if ($role) {
             $role->syncPermissions(Permission::find(array_keys($this->selectedpermissions))->pluck('name'));
-            $this->selectedpermissions = $role->getAllPermissions()->sortBy('name')
-                ->pluck('name', 'id')
-                ->toArray();
+            $this->selectedpermissions=$role->permissions->pluck('id','id')->toArray();
             $this->emit('saved');
         }
     }    
 
-    public function render()
-    {
-        $roles = Role::all();
-        $permissions = Permission::all()->sortBy('name');
-            //->pluck('id', 'id');
-        return view('livewire.permissions.index', compact('roles', 'permissions'));
+    public function createRole(){
+        //dd($this->newrole);
+        // check if $this->newrole exists in Roles table
+        $role=Role::where('name',$this->newrole)->first();
+        if (!$role) {
+            $role=Role::create(['name'=>$this->newrole]);
+            $this->newrole='';
+            $this->emit('toast','Registro Guardado','success');
+        } else{
+            $this->emit('toast','Rol existente','warning');
+        }
     }
+
+    public function createPermission(){
+        // check if $this->newpermission exists in Permissions table
+        $permission=Permission::where('name', $this->newpermission)->first();
+        if (!$permission) {
+            $permission=Permission::create(['name'=>$this->newpermission]);
+            $this->newpermission='';
+            $this->emit('toast', 'Registro Guardado', 'success');
+        } else {
+            $this->emit('toast', 'Permiso existente', 'warning');
+        }
+    }
+
 }
--- resources/views/livewire/permissions/index.blade.php
+++ resources/views/livewire/permissions/index.blade.php
@@ -5,12 +5,31 @@
     </x-slot>
     <x-slot name="description">
       Administrar Roles y permisos
+
+      <div class="flex flex-wrap">
+      <x-jet-input class="w-3/4" type="text" wire:model.lazy="newrole" placeholder="Nombre del Rol" />
+      <x-jet-button wire:loading.attr="disabled" wire:click="createRole" class="my-1">
+        <svg xmlns="http://www.w3.org/2000/svg" width="16" height="16" fill="currentColor" class="bi bi-save" viewBox="0 0 16 16">
+          <path d="M2 1a1 1 0 0 0-1 1v12a1 1 0 0 0 1 1h12a1 1 0 0 0 1-1V2a1 1 0 0 0-1-1H9.5a1 1 0 0 0-1 1v7.293l2.646-2.647a.5.5 0 0 1 .708.708l-3.5 3.5a.5.5 0 0 1-.708 0l-3.5-3.5a.5.5 0 1 1 .708-.708L7.5 9.293V2a2 2 0 0 1 2-2H14a2 2 0 0 1 2 2v12a2 2 0 0 1-2 2H2a2 2 0 0 1-2-2V2a2 2 0 0 1 2-2h2.5a.5.5 0 0 1 0 1H2z"/>
+        </svg>
+      </x-jet-button>
+      </div>
+
+      <div class="flex flex-wrap">
+      <x-jet-input class="w-3/4" type="text" wire:model.lazy="newpermission" placeholder="Nombre del Permiso" />
+      <x-jet-button wire:loading.attr="disabled" wire:click="createPermission" class="my-1">
+        <svg xmlns="http://www.w3.org/2000/svg" width="16" height="16" fill="currentColor" class="bi bi-save" viewBox="0 0 16 16">
+          <path d="M2 1a1 1 0 0 0-1 1v12a1 1 0 0 0 1 1h12a1 1 0 0 0 1-1V2a1 1 0 0 0-1-1H9.5a1 1 0 0 0-1 1v7.293l2.646-2.647a.5.5 0 0 1 .708.708l-3.5 3.5a.5.5 0 0 1-.708 0l-3.5-3.5a.5.5 0 1 1 .708-.708L7.5 9.293V2a2 2 0 0 1 2-2H14a2 2 0 0 1 2 2v12a2 2 0 0 1-2 2H2a2 2 0 0 1-2-2V2a2 2 0 0 1 2-2h2.5a.5.5 0 0 1 0 1H2z"/>
+        </svg>
+      </x-jet-button>
+      </div>
+
+
+
     </x-slot>
     <x-slot name="form">
       @include('livewire.permissions.roles')
-
       @include('livewire.permissions.permissions')
-
     </x-slot>
 
     <x-slot name="actions">
@@ -19,7 +38,9 @@
       </x-jet-action-message>
 
       <x-jet-button wire:loading.attr="disabled" wire:target="selectedrole">
-        {{ __('Save') }}
+        <svg xmlns="http://www.w3.org/2000/svg" class="h-6 w-6" fill="none" viewBox="0 0 24 24" stroke="currentColor">
+          <path stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="M5 13l4 4L19 7" />
+        </svg>&nbsp;Guardar
       </x-jet-button>
     </x-slot>
   </x-jet-form-section>
