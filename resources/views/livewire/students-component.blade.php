67b7adbbcadf3851ecb6205eeaf1b83e6325838e
quileab <quileab@gmail.com>
2022-04-23 08:19:17 -0300

    Added SVG icons as components

--- resources/views/livewire/students-component.blade.php
+++ resources/views/livewire/students-component.blade.php
@@ -225,16 +225,11 @@
                 </a>
                 <button class="bg-yellow-600 text-white px-3 py-2 rounded-md"
                   wire:click="$emit('setBookmark','{{ $student->id }}')">
-                  <svg xmlns="http://www.w3.org/2000/svg" class="h-6 w-6" fill="currentColor" class="bi bi-bookmark-plus" viewBox="0 0 16 16">
-                    <path d="M2 2a2 2 0 0 1 2-2h8a2 2 0 0 1 2 2v13.5a.5.5 0 0 1-.777.416L8 13.101l-5.223 2.815A.5.5 0 0 1 2 15.5V2zm2-1a1 1 0 0 0-1 1v12.566l4.723-2.482a.5.5 0 0 1 .554 0L13 14.566V2a1 1 0 0 0-1-1H4z"/>
-                    <path d="M8 4a.5.5 0 0 1 .5.5V6H10a.5.5 0 0 1 0 1H8.5v1.5a.5.5 0 0 1-1 0V7H6a.5.5 0 0 1 0-1h1.5V4.5A.5.5 0 0 1 8 4z"/>
-                  </svg>
+                  <x-svg.bookmarkPlus class="h-6 w-6" />
                 </button>
                 <button class="bg-red-600 text-white px-3 py-2 rounded-md"
                   wire:click="$emit('confirmDelete','{{ $student->lastname }}, {{ $student->firstname }}','{{ $student->id }}','delete')">
-                  <svg xmlns="http://www.w3.org/2000/svg" class="h-7 w-7" fill="none" viewBox="0 0 24 24" stroke="currentColor">
-                    <path stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="M19 7l-.867 12.142A2 2 0 0116.138 21H7.862a2 2 0 01-1.995-1.858L5 7m5 4v6m4-6v6m1-10V4a1 1 0 00-1-1h-4a1 1 0 00-1 1v3M4 7h16" />
-                  </svg>
+                  <x-svg.trash class="h-6 w-6" />
                 </button>
               </td>
             </tr>
